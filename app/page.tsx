10cfb48ed96a8307aaa5ec124c6cc01e4471c1ec
yuyeapp <yuye.app@gmail.com>
2024-09-24 21:10:20 -0400

    Chapter 3: optimizing Fonts and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -29,8 +30,28 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <HeroImage />
         </div>
       </div>
     </main>
+  );
+}
+
+function HeroImage() {
+  return (
+    <>
+      <Image
+        // src='/hero-desktop.png'
+        src='https://images.pexels.com/photos/20103701/pexels-photo-20103701/free-photo-of-a-group-of-men-sitting-in-a-park.jpeg'
+        width={1000}
+        height={760}
+        className='hidden md:block'
+        alt={'Screenshots of the dashboard project showing desktop version'} />
+      <Image src={'/hero-mobile.png'}
+        width={560}
+        height={620}
+        className='block md:hidden'
+        alt={'Screenshot of the dashboard project showing mobile version'} />
+    </>
   );
 }
---
+++ note.md
@@ -0,0 +1,5 @@
+## Chapter 3: optimizing Fonts and images
+
+[Link](https://nextjs.org/learn/dashboard-app/optimizing-fonts-images)
+
+Image Optimization: https://nextjs.org/docs/app/building-your-application/optimizing/images
