91624da902646e469999fadf091db0db00e4c7be
yuyeapp <yuye.app@gmail.com>
2024-09-25 21:24:48 -0400

    Chapter 7: Fetching data [Part 1]

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,24 +8,29 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import { users, customers, invoices, revenue } from './placeholder-data'
+
+// export async function fetchRevenue() {
+//   try {
+//     // Artificially delay a response for demo purposes.
+//     // Don't do this in production :)
+
+//     // console.log('Fetching revenue data...');
+//     // await new Promise((resolve) => setTimeout(resolve, 3000));
+
+//     const data = await sql<Revenue>`SELECT * FROM revenue`;
+
+//     // console.log('Data fetch completed after 3 seconds.');
+
+//     return data.rows;
+//   } catch (error) {
+//     console.error('Database Error:', error);
+//     throw new Error('Failed to fetch revenue data.');
+//   }
+// }
 
 export async function fetchRevenue() {
-  try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data.rows;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch revenue data.');
-  }
+  return revenue;
 }
 
 export async function fetchLatestInvoices() {
