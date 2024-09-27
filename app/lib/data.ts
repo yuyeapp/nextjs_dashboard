e5f15b6b5dc28ef26ac48f3c313b148e28f868ba
yuyeapp <yuye.app@gmail.com>
2024-09-26 21:11:55 -0400

    Chapter 9: Streaming [Part 1 - Loading]

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -30,6 +30,9 @@ import { users, customers, invoices, revenue, latestInvoces } from './placeholde
 // }
 
 export async function fetchRevenue() {
+  console.log('Fetching revenue data...');
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  console.log('Data fetch completed after 3 seconds.');
   return revenue;
 }
 
