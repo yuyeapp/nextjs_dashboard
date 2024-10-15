ecbb98b6a6e71b978db6abf9b7f0781a1c691504
yuyeapp <yuye.app@gmail.com>
2024-10-14 22:22:59 -0400

    Chapter 11: Pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -161,25 +161,34 @@ export async function fetchFilteredInvoices(
   return filteredInvoices.slice(offset, offsetEnd);
 }
 
-export async function fetchInvoicesPages(query: string) {
-  try {
-    const count = await sql`SELECT COUNT(*)
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE
-      customers.name ILIKE ${`%${query}%`} OR
-      customers.email ILIKE ${`%${query}%`} OR
-      invoices.amount::text ILIKE ${`%${query}%`} OR
-      invoices.date::text ILIKE ${`%${query}%`} OR
-      invoices.status ILIKE ${`%${query}%`}
-  `;
+// export async function fetchInvoicesPages(query: string) {
+//   try {
+//     const count = await sql`SELECT COUNT(*)
+//     FROM invoices
+//     JOIN customers ON invoices.customer_id = customers.id
+//     WHERE
+//       customers.name ILIKE ${`%${query}%`} OR
+//       customers.email ILIKE ${`%${query}%`} OR
+//       invoices.amount::text ILIKE ${`%${query}%`} OR
+//       invoices.date::text ILIKE ${`%${query}%`} OR
+//       invoices.status ILIKE ${`%${query}%`}
+//   `;
 
-    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
-    return totalPages;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch total number of invoices.');
+//     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+//     return totalPages;
+//   } catch (error) {
+//     console.error('Database Error:', error);
+//     throw new Error('Failed to fetch total number of invoices.');
+//   }
+// }
+
+export async function fetchInvoicesPages(query: string) {
+  if (query) {
+    return Math.ceil(filteredInvoices
+      .filter(q => q.name.includes(query) || q.email.includes(query) || q.status.includes(query))
+      .length / ITEMS_PER_PAGE);
   }
+  return Math.ceil(filteredInvoices.length / ITEMS_PER_PAGE);
 }
 
 export async function fetchInvoiceById(id: string) {
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,6 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     console.log(`Searching... ${term}`);
     if (term) {
       params.set('query', term);
