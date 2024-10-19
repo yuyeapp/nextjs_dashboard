68491048b1e480102f025857e626c1750062b780
yuyeapp <yuye.app@gmail.com>
2024-10-19 17:00:24 -0400

    Chapter 12: Mutating Data

---
+++ app/lib/actions.ts
@@ -0,0 +1,35 @@
+'use server';
+
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    // set to coerce (change) from a string to a number while also validating its type.
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoiceSchema = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoiceSchema.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    // good practice to store monetary values in cents in your database to eliminate
+    // JavaScript floating-point errors and ensure greater accuracy.
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+    console.log(customerId, amount, status, amountInCents, date);
+
+    // TODO: insert into db
+
+    // clear this cache and trigger a new request to the server
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -216,22 +216,25 @@ export async function fetchInvoiceById(id: string) {
   }
 }
 
-export async function fetchCustomers() {
-  try {
-    const data = await sql<CustomerField>`
-      SELECT
-        id,
-        name
-      FROM customers
-      ORDER BY name ASC
-    `;
+// export async function fetchCustomers() {
+//   try {
+//     const data = await sql<CustomerField>`
+//       SELECT
+//         id,
+//         name
+//       FROM customers
+//       ORDER BY name ASC
+//     `;
 
-    const customers = data.rows;
-    return customers;
-  } catch (err) {
-    console.error('Database Error:', err);
-    throw new Error('Failed to fetch all customers.');
-  }
+//     const customers = data.rows;
+//     return customers;
+//   } catch (err) {
+//     console.error('Database Error:', err);
+//     throw new Error('Failed to fetch all customers.');
+//   }
+// }
+export async function fetchCustomers() {
+  return customers;
 }
 
 export async function fetchFilteredCustomers(query: string) {
